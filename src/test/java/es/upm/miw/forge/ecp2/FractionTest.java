feada4cbabbc393a80d1d16dbf5c9c3433acade3
bo0410 <bo0410>
2017-09-20 23:04:24 +0200

    #4: Método isEquivalent() + test

--- src/test/java/es/upm/miw/forge/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/forge/ecp2/FractionTest.java
@@ -8,10 +8,13 @@ import org.junit.Test;
 public class FractionTest {
 
     private Fraction fraction;
+    private Fraction fraction2;
+    private Fraction fraction3;
     
     @Before
     public void before(){
-        fraction = new Fraction();
+        fraction = new Fraction(2,6);
+        fraction3 = new Fraction(1,3);
     }
 
     @Test
@@ -32,6 +35,14 @@ public class FractionTest {
     public void testDecimal() {
         fraction = new Fraction(4,8);
         assertEquals(0.5, fraction.decimal(),0.5);
+    }
+    
+    @Test
+    public void testIsEquivalent() {
+        fraction2 = new Fraction(1,3);
+        assertEquals(true,fraction.isEquivalent(fraction2));
+        fraction3 = new Fraction(5,7);
+        assertEquals(false,fraction.isEquivalent(fraction3));
     }
 
 }
